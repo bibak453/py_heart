18ab95495dc30efd33b2fd2ec80e8549e55b2f45
bibak453 <xdjveugs7@proton.me>
2024-02-13 01:30:22 +0100

    testing, testing

--- pyTest/flags.py
+++ pyTest/flags.py
@@ -26,29 +26,4 @@ class FlagManager:
         else:
             self.flags[flag_address] &= ~(1 << bit_position)
 
-    def print_flags(self):
-        print("Current Flags:")
-        for flag_address, value in self.flags.items():
-            print(f"Flag {flag_address}: {value}")
-
-
-# Example usage:
 manager = FlagManager()
-
-# Set flag 'a6' to 1
-manager.set_flag('a6', 1)
-
-# Add 3 to flag '1d'
-manager.add_to_flag('1d', 3)
-
-# Subtract 2 from flag '18'
-manager.subtract_from_flag('18', 2)
-
-# Add 1 to flag 'af'
-manager.add_to_flag('af', 1)
-
-# Set bit 2 of flag '04' to 1
-manager.set_bit_flag('04', 2, 1)
-
-# Print current flags
-manager.print_flags()
